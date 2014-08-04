f6fd433d1f6b7b86f8bc39e6905d6876b41e94cd
vortex <simone.murzilli@fao.org>
2014-08-04 11:18:37 +0200

    initliaze project

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from distutils.core import setup
 
 setup(
-    name='TowelStuff',
+    name='PGeoREST',
     version='0.1.0',
-    author='J. Random Hacker',
-    author_email='jrh@example.com',
-    packages=['towelstuff', 'towelstuff.test'],
-    scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    url='http://pypi.python.org/pypi/TowelStuff/',
+    author='Simone Murzilli; Guido Barbaglia',
+    author_email='geobrickspy@gmail.com',
+    packages=['pgeorest'],
+    # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
+    url='http://pypi.python.org/pypi/PGeoREST/',
     license='LICENSE.txt',
-    description='Useful towel-related stuff.',
+    description='PGeoREST module.',
     long_description=open('README.txt').read(),
     install_requires=[
-        "Django >= 1.1.1",
-        "caldav == 0.1.4",
-    ],
-)
+        "flask >= 0.10.1",
+        "flask-cors >= 1.3.1",
+        # "pgeo >= 0.1.0",
+        ]
+    )
