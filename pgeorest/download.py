bfd9fb22e2658da61d6ce3cd8441ac4fd6636ff1
Guido Barbaglia <guido.barbaglia@gmail.com>
2014-08-26 16:39:38 +0200

    Download multiple days at once.

--- pgeorest/download.py
+++ pgeorest/download.py
@@ -7,12 +7,14 @@ from flask import Response
 from pgeo.error.custom_exceptions import PGeoException
 from pgeo.thread.download_threads_manager import Manager
 from pgeo.thread.download_threads_manager import progress_map
+from pgeo.thread.download_threads_manager import multi_progress_map
 from pgeo.thread.download_threads_manager import out_template
 from pgeo.gis.raster import process_hdfs
 from pgeo.config.settings import read_config_file_json
 
 
 download = Blueprint('download', __name__)
+managers = {}
 
 
 @download.route('/')
@@ -28,7 +30,10 @@ def manager_start(source_name):
         payload = request.get_json()
         file_paths_and_sizes = payload['file_paths_and_sizes']
         filesystem_structure = payload['filesystem_structure']
-        mgr = Manager(source_name, file_paths_and_sizes, filesystem_structure)
+        tab_id = payload['tab_id']
+        if tab_id not in managers:
+            managers[tab_id] = {}
+        mgr = Manager(source_name, file_paths_and_sizes, filesystem_structure, tab_id)
         target_dir = mgr.run()
         out = {'source_path': target_dir}
         return Response(json.dumps(out), content_type='application/json; charset=utf-8')
@@ -43,6 +48,17 @@ def progress(layer_name):
     if layer_name not in progress_map:
         return jsonify(progress=out_template)
     return jsonify(progress_map[layer_name])
+
+
+@download.route('/progress/<tab_index>/<layer_name>')
+@download.route('/progress/<tab_index>/<layer_name>/')
+@cross_origin(origins='*')
+def multiple_progress(layer_name, tab_index):
+    if tab_index not in multi_progress_map:
+        return jsonify(progress=out_template)
+        if layer_name not in multi_progress_map[tab_index]:
+            return jsonify(progress=out_template)
+    return jsonify(multi_progress_map[tab_index][layer_name])
 
 
 @download.route('/process/<source_name>', methods=['POST'])
