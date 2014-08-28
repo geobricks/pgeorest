94ce55da88f56c2fc4c10e7c48dab7cd7ad4a64e
vortex <simone.murzilli@fao.org>
2014-08-28 16:40:10 +0200

    added authority code for the extend query (should be the same as the raster one)

--- pgeorest/distribution.py
+++ pgeorest/distribution.py
@@ -9,7 +9,7 @@ from flask import request, send_from_directory
 from shutil import move
 import os
 import uuid
-from pgeo.gis.raster import crop_by_vector_database
+from pgeo.gis.raster import crop_by_vector_database, get_authority
 from pgeo.stats.raster import Stats
 from pgeo.utils.filesystem import create_folder_in_tmp, get_filename, zip_files
 from pgeo.utils import email_utils
@@ -32,6 +32,8 @@ def index():
     return 'Welcome to the distribution module!'
 
 
+#http://localhost:5005/distribution/raster/fenix:trmm_06_2014;fenix:trmm_07_2014;fenix:trmm_08_2014;fenix:rice_area1;fenix:apple_area1;fenix:banana_area1/spatial_query/{ "vector" : { "query_extent" : "SELECT ST_AsGeoJSON(ST_Extent(geom)) FROM {{SCHEMA}}.gaul0_3857_test WHERE adm0_name IN ('Spain', 'Italy', 'Germany','China')", "query_layer" : "SELECT * FROM {{SCHEMA}}.gaul0_3857_test WHERE adm0_name IN ('Spain', 'Italy', 'Germany','China')"}}
+#http://localhost:5005/distribution/raster/fenix:MODIS_brazil_korea/spatial_query/%7B%20%22vector%22%20:%20%7B%20%22query_extent%22%20:%20%22SELECT%20ST_AsGeoJSON%28ST_Transform%28ST_SetSRID%28ST_Extent%28geom%29,%203857%29,%20%7B%7BSRID%7D%7D%29%29%20FROM%20%7B%7BSCHEMA%7D%7D.gaul0_3857_test%20WHERE%20adm0_name%20IN%20%28%27Brazil%27%29%22,%20%22query_layer%22%20:%20%22SELECT%20*%20FROM%20%7B%7BSCHEMA%7D%7D.gaul0_3857_test%20WHERE%20adm0_name%20IN%20%28%27Brazil%27%29%22%7D%7D
 @app.route('/raster/<uids>/spatial_query/<spatial_query>/', methods=['GET'])
 @app.route('/raster/<uids>/spatial_query/<spatial_query>', methods=['GET'])
 @cross_origin(origins='*', headers=['Content-Type'])
@@ -57,16 +59,22 @@ def get_layers(uids, spatial_query):
         for uid in uids:
             log.info(uid)
             raster_path = stats.get_raster_path(uid)
+
+            authority_name, authority_code  = get_authority(raster_path)
             log.info(db_spatial.schema)
+            log.info(authority_name)
+            log.info(authority_code)
+
+
             query_extent = json_filter["vector"]["query_extent"]
             query_layer = json_filter["vector"]["query_layer"]
 
             query_extent = query_extent.replace("{{SCHEMA}}", db_spatial.schema)
+            query_extent = query_extent.replace("{{SRID}}", authority_code)
             query_layer = query_layer.replace("{{SCHEMA}}", db_spatial.schema)
 
             # create the file on tm folder
             filepath = crop_by_vector_database(raster_path, db_spatial, query_extent, query_layer)
-
 
             # move file to distribution tmp folder
             path, filename, name = get_filename(filepath, True)
