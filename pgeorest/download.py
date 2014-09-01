616def23526924ca38616971dc29e6ec00266614
guidofenix <guido.barbaglia@fao.org>
2014-09-01 16:20:53 +0200

    Working on TRMM thread manager.

--- pgeorest/download.py
+++ pgeorest/download.py
@@ -6,6 +6,8 @@ from flask import request
 from flask import Response
 from pgeo.error.custom_exceptions import PGeoException
 from pgeo.thread.download_threads_manager import Manager
+from pgeo.thread.bulk_download_threads_manager import BulkDownloadManager
+from pgeo.thread.bulk_download_threads_manager import progress_map as bulk_progress_map
 from pgeo.thread.download_threads_manager import multi_progress_map
 from pgeo.thread.download_threads_manager import out_template
 from pgeo.gis.raster import process_hdfs
@@ -62,14 +64,39 @@ def process_rasters_service(source_name):
         obj = conf['processing']
         obj['source_path'] = payload['source_path']
         obj['output_path'] = payload['source_path'] + '/OUTPUT'
-        print 'PROCESS TO ' + str(obj['output_path'])
         obj['gdalwarp']['-tr'] = str(payload['pixel_size']) + ', -' + str(payload['pixel_size'])
         try:
             tiff = process_hdfs(obj)
             return Response(json.dumps('{"TIFF":"' + tiff + '"}'), content_type='application/json; charset=utf-8')
         except Exception, e:
-            # raise PGeoException(e.message, 500)
             return Response(json.dumps('{"Message":"' + e.message + '"}'), content_type='application/json; charset=utf-8')
     except Exception, e:
-        # raise PGeoException(e.message, 500)
         return Response(json.dumps('{"Message":"' + e.message + '"}'), content_type='application/json; charset=utf-8')
+
+
+@download.route('/bulk/<source_name>', methods=['POST'])
+@download.route('/bulk/<source_name>/', methods=['POST'])
+@cross_origin(origins='*', headers=['Content-Type'])
+def bulk_manager_start(source_name):
+    try:
+        payload = request.get_json()
+        filesystem_structure = payload['filesystem_structure']
+        bulk_download_objects = payload['bulk_download_objects']
+        tab_id = payload['tab_id']
+        mgr = BulkDownloadManager(source_name, filesystem_structure, bulk_download_objects, tab_id)
+        target_dir = mgr.run()
+        out = {'source_path': target_dir}
+        return Response(json.dumps(out), content_type='application/json; charset=utf-8')
+    except Exception, e:
+        print e
+        raise PGeoException(e.message, 500)
+
+
+@download.route('/bulk/progress/<tab_index>')
+@download.route('/bulk/progress/<tab_index>/')
+@cross_origin(origins='*')
+def bulk_progress(tab_index):
+    print bulk_progress_map
+    if tab_index not in bulk_progress_map:
+        return jsonify(progress=out_template)
+    return jsonify(bulk_progress_map[tab_index])
