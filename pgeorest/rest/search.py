5b3204a9e46a814ebcf4614bbdfae910badf8ed6
vortex <simone.murzilli@fao.org>
2014-10-28 10:52:53 +0100

    added dbname as first parameter on the search

--- pgeorest/rest/search.py
+++ pgeorest/rest/search.py
@@ -7,11 +7,16 @@ from pgeorest.config.settings import settings
 
 
 search = Blueprint('search', __name__)
-connection = settings['db']['metadata']['connection']
-db = settings['db']['metadata']['database']
-doc = settings['db']['metadata']['document']['layer']
+
 # mongo_search = MongoSearch(connection, db, doc)
 
+
+def get_connection_properties(dbname):
+    connection = settings['db'][dbname]['connection']
+    db = settings['db'][dbname]['database']
+    doc = settings['db'][dbname]['document']['layer']
+    return connection, db, doc
+    
 
 @search.route('/')
 @cross_origin(origins='*')
@@ -19,83 +24,96 @@ def index():
     return 'Welcome to the Schema module!'
 
 
-@search.route('/layer/id/<id>', methods=['GET'])
-@search.route('/layer/id/<id>/', methods=['GET'])
+@search.route('/<dbname>/layer/id/<id>', methods=['GET'])
+@search.route('/<dbname>/layer/id/<id>/', methods=['GET'])
 @cross_origin(origins='*')
-def find_layer_by_id_service(id):
+def find_layer_by_id_service(dbname, id):
+    connection, db, doc = get_connection_properties(dbname)
     mongo_search = MongoSearch(connection, db, doc)
     out = json_util.dumps(mongo_search.find_layer_by_id(id))
     return Response(out, content_type='application/json; charset=utf-8')
 
 
-@search.route('/layer/dekad/<dekad>', methods=['GET'])
-@search.route('/layer/dekad/<dekad>/', methods=['GET'])
+@search.route('/<dbname>/layer/dekad/<dekad>', methods=['GET'])
+@search.route('/<dbname>/layer/dekad/<dekad>/', methods=['GET'])
 @cross_origin(origins='*')
-def find_layer_by_dekad_service(dekad):
+def find_layer_by_dekad_service(dbname, dekad):
+    connection, db, doc = get_connection_properties(dbname)
     mongo_search = MongoSearch(connection, db, doc)
     out = json_util.dumps(mongo_search.find_layers_by_product(None, dekad, None))
     return Response(out, content_type='application/json; charset=utf-8')
 
 
-@search.route('/layer/product/<product>', methods=['GET'])
-@search.route('/layer/product/<product>/', methods=['GET'])
+@search.route('/<dbname>/layer/product/<product>', methods=['GET'])
+@search.route('/<dbname>/layer/product/<product>/', methods=['GET'])
 @cross_origin(origins='*')
-def find_layer_by_product_service(product):
+def find_layer_by_product_service(dbname, product):
+    connection, db, doc = get_connection_properties(dbname)
     mongo_search = MongoSearch(connection, db, doc)
     out = json_util.dumps(mongo_search.find_layers_by_product(product, None, None))
     return Response(out, content_type='application/json; charset=utf-8')
 
 
-@search.route('/layer/product/<product>/type/<aggregation_type>', methods=['GET'])
-@search.route('/layer/product/<product>/type/<aggregation_type>/', methods=['GET'])
+@search.route('/<dbname>/layer/product/<product>/type/<aggregation_type>', methods=['GET'])
+@search.route('/<dbname>/layer/product/<product>/type/<aggregation_type>/', methods=['GET'])
 @cross_origin(origins='*')
-def find_layer_by_product_and_type_service(product, aggregation_type):
+def find_layer_by_product_and_type_service(dbname, product, aggregation_type):
+    connection, db, doc = get_connection_properties(dbname)
     mongo_search = MongoSearch(connection, db, doc)
     out = json_util.dumps(mongo_search.find_layers_by_product(product, None, aggregation_type))
     return Response(out, content_type='application/json; charset=utf-8')
 
 
 
-@search.route('/layer/product/<product>/dekad/<dekad>', methods=['GET'])
-@search.route('/layer/product/<product>/dekad/<dekad>/', methods=['GET'])
+@search.route('/<dbname>/layer/product/<product>/dekad/<dekad>', methods=['GET'])
+@search.route('/<dbname>/layer/product/<product>/dekad/<dekad>/', methods=['GET'])
 @cross_origin(origins='*')
-def find_layer_by_product_and_dekad_service(product, dekad):
+def find_layer_by_product_and_dekad_service(dbname, product, dekad):
+    connection, db, doc = get_connection_properties(dbname)
     mongo_search = MongoSearch(connection, db, doc)
     out = json_util.dumps(mongo_search.find_layers_by_product(product, dekad, None))
     return Response(out, content_type='application/json; charset=utf-8')
 
 
-@search.route('/layer/product/<product>/dekad/<dekad>/type/<aggregation_type>', methods=['GET'])
-@search.route('/layer/product/<product>/dekad/<dekad>/type/<aggregation_type>/', methods=['GET'])
+@search.route('/<dbname>/layer/product/<product>/dekad/<dekad>/type/<aggregation_type>', methods=['GET'])
+@search.route('/<dbname>/layer/product/<product>/dekad/<dekad>/type/<aggregation_type>/', methods=['GET'])
 @cross_origin(origins='*')
-def find_layer_by_product_and_dekad_and_type_service(product, dekad, aggregation_type):
+def find_layer_by_product_and_dekad_and_type_service(dbname, product, dekad, aggregation_type):
+    connection, db, doc = get_connection_properties(dbname)
     mongo_search = MongoSearch(connection, db, doc)
     out = json_util.dumps(mongo_search.find_layers_by_product(product, dekad, aggregation_type))
     return Response(out, content_type='application/json; charset=utf-8')
 
 
-@search.route('/layer/from/<dekad_from>/to/<dekad_to>/product/<product>', methods=['GET'])
-@search.route('/layer/from/<dekad_from>/to/<dekad_to>/product/<product>/', methods=['GET'])
+@search.route('/<dbname>/layer/from/<dekad_from>/to/<dekad_to>/product/<product>', methods=['GET'])
+@search.route('/<dbname>/layer/from/<dekad_from>/to/<dekad_to>/product/<product>/', methods=['GET'])
 @cross_origin(origins='*')
 def find_layer_by_dekad_range(dekad_from, dekad_to, product):
+    connection, db, doc = get_connection_properties(dbname)
     mongo_search = MongoSearch(connection, db, doc)
     out = json_util.dumps(mongo_search.find_layers_by_dekad_range(dekad_from, dekad_to, product))
     return Response(out, content_type='application/json; charset=utf-8')
 
 
-@search.route('/layer/from/<dekad_from>/to/<dekad_to>', methods=['GET'])
-@search.route('/layer/from/<dekad_from>/to/<dekad_to>/', methods=['GET'])
+@search.route('/<dbname>/layer/from/<dekad_from>/to/<dekad_to>', methods=['GET'])
+@search.route('/<dbname>/layer/from/<dekad_from>/to/<dekad_to>/', methods=['GET'])
 @cross_origin(origins='*')
-def find_layer_by_dekad_range_and_product(dekad_from, dekad_to):
+def find_layer_by_dekad_range_and_product(dbname, dekad_from, dekad_to):
+    connection, db, doc = get_connection_properties(dbname)
     mongo_search = MongoSearch(connection, db, doc)
     out = json_util.dumps(mongo_search.find_layers_by_dekad_range(dekad_from, dekad_to, None))
     return Response(out, content_type='application/json; charset=utf-8')
 
 
-@search.route('/layer/distinct/<field>', methods=['GET'])
-@search.route('/layer/distinct/<field>/', methods=['GET'])
+@search.route('/<dbname>/layer/distinct/<field>', methods=['GET'])
+@search.route('/<dbname>/layer/distinct/<field>/', methods=['GET'])
 @cross_origin(origins='*')
-def find_distinct(field):
+def find_distinct(dbname, field):
+    connection, db, doc = get_connection_properties(dbname)
     mongo_search = MongoSearch(connection, db, doc)
     out = json_util.dumps(mongo_search.find_all(field))
     return Response(out, content_type='application/json; charset=utf-8')
+
+
+
+    
